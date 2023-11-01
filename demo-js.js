a773de5b505922d9966eaa1629d1d27918e5302e
alfieclouddevs <145130714+alfieclouddevs@users.noreply.github.com>
2023-11-01 16:42:56 +0000

    Update demo-js.js

--- demo-js.js
+++ demo-js.js
@@ -1,5 +1,16 @@
 console.log("goodbye")
 
+const timesToRepeat = 10;
+const character = ':)';
+
+for (let i = 0; i<= timesToRepeat; i++){
+    
+}
+let answer = '';
+
+for (let i = 0; i< timesToRepeat; i++){
+    
+}
 
 
-
+console.log (answer);
