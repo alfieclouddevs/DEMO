bc6d7def053373f3b189ccbef87acd5b6864b25f
alfieclouddevs <145130714+alfieclouddevs@users.noreply.github.com>
2023-11-24 13:18:47 +0000

    l

--- demo-js.js
+++ demo-js.js
@@ -1,2 +1,28 @@
 console.log('hello');
 
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent*12;
+
+console.log(yearlyRent);
+
+function addTwo(number){
+    return number +2;
+}
+
+const finalAnswer = addTwo(5);
+console.log(finalAnswer);
+
+const otherAnswer = addTwo(10);
+console.log(otherAnswer);
+
+const person = {
+    firstName:"Brian",
+    surname:"Holt",
+    city:"Seattle",
+
+};
+
+const propertyName = 'city';
+console.log(person["propertyName"]);
+console.log(firstName);
